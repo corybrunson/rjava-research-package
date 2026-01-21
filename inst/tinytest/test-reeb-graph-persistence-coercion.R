dcd6c105d3619e74c10e4f24702ece3fc54d3eab
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2026-01-21 18:06:58 -0500

    condition igraph & network tests + specify versions required

--- inst/tinytest/test-reeb-graph-persistence-coercion.R
+++ inst/tinytest/test-reeb-graph-persistence-coercion.R
@@ -6,11 +6,15 @@ x <- read_reeb_graph(f)
 x_ph <- reeb_graph_persistence(x)
 
 # igraph
-g <- as_igraph(x, values = "height")
-g_ph <- reeb_graph_persistence(g, values = "height")
-expect_equivalent(x_ph, g_ph)
+if (rlang::is_installed("igraph")) {
+  g <- as_igraph(x, values = "height")
+  g_ph <- reeb_graph_persistence(g, values = "height")
+  expect_equivalent(x_ph, g_ph)
+}
 
 # network
-net <- as_network(x, values = "height")
-net_ph <- reeb_graph_persistence(net, values = "height")
-expect_equivalent(x_ph, net_ph)
+if (rlang::is_installed("network")) {
+  net <- as_network(x, values = "height")
+  net_ph <- reeb_graph_persistence(net, values = "height")
+  expect_equivalent(x_ph, net_ph)
+}
